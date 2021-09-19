978ee286a6a51b1f9e1debf1007aa76a6d9bd324
shrutisg2410 <shrutisg2410@gmail.com>
2021-09-19 22:47:12 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,16 @@ import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { celciussuffix } from './celciussuffix.pipe';
 import { NgxSpinnerModule } from 'ngx-spinner';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, NgxSpinnerModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    NgxSpinnerModule,
+    BrowserAnimationsModule,
+  ],
   declarations: [AppComponent, HelloComponent, celciussuffix],
   bootstrap: [AppComponent],
 })
