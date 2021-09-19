5cd0a7159675b87924cd232c18d8d41867df9415
shrutisg2410 <shrutisg2410@gmail.com>
2021-09-19 22:32:12 +0530

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HttpClientModule } from '@angular/common/http';
 import { celciussuffix } from './celciussuffix.pipe';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule, NgxSpinnerModule],
   declarations: [AppComponent, HelloComponent, celciussuffix],
   bootstrap: [AppComponent],
 })
